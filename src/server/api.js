363e112f0f14753baf839beb02089c156d7e707a
Jang Haemin <io@jhaemin.com>
2019-08-15 21:12:00 +0900

    Error handling

--- src/server/api.js
+++ src/server/api.js
@@ -1,19 +1,35 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/api/daily-list', async (req, response) => {
+router.get('/api/daily-list', (req, response) => {
   const fetchDailyList = require('./modules/fetch-daily-list.js')
 
-  let cartoons = await fetchDailyList(req.query.day)
-  response.json(cartoons)
+  fetchDailyList(req.query.day)
+    .then((cartoons) => {
+      response.json(cartoons)
+    })
+    .catch((error) => {
+      console.error(error)
+      response.json({
+        error: true
+      })
+    })
 })
 
 // Fetch episodes
-router.get('/api/episodes', async (req, response) => {
+router.get('/api/episodes', (req, response) => {
   const fetchEpisodes = require('./modules/fetch-episodes.js')
 
-  let episodes = await fetchEpisodes(req.query.cartoonId)
-  response.json(episodes)
+  fetchEpisodes(req.query.cartoonId)
+    .then((episodes) => {
+      response.json(episodes)
+    })
+    .catch((error) => {
+      console.error(error)
+      response.json({
+        error: true
+      })
+    })
 })
 
 module.exports = router
